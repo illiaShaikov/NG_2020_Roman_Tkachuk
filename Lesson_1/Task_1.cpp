da4176a08640f8d2140e90bac1d7b4aa2f184aba
Roman Tkachuk <tkachukroman64@gmail.com>
2020-02-22 22:12:39 +0200

    Added first lection

---
+++ Lesson_1/Task_1.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+
+// Shpora
+// cout << "chtoto"; <-- выведи что-то
+// cin >> a; <-- Положи что-то в a
+// cout << a; <-- Выведи то, что лежит в a
+// int a; <-- Создай ящичек a
+// if (условие) { код; код; код; }
+int main()
+{
+}
